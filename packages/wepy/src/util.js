a4a7ef8d1410fd9516e9e5b59c7a09b983c533d0
max <545939948@qq.com>
2018-09-13 09:38:10 +0800

    fix:临时修复issues #1767

--- packages/wepy/src/util.js
+++ packages/wepy/src/util.js
@@ -1,7 +1,7 @@
 /**
  * Tencent is pleased to support the open source community by making WePY available.
  * Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
- * 
+ *
  * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  * http://opensource.org/licenses/MIT
  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
@@ -202,7 +202,7 @@ export default {
     $copy (obj, deep = false) {
         if (Array.isArray(obj)) {
             return this.$extend(deep, [], obj);
-        } else if ('' + obj === 'null') {
+        } else if (obj === null) {
             return obj;
         } else if (typeof (obj) === 'object') {
             return this.$extend(deep, {}, obj);
@@ -303,5 +303,3 @@ export default {
         return str.replace(/-(\w)/g, (_, c) => c ? c.toUpperCase() : '');
     }
 };
-
-
